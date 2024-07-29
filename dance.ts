6c6229d1e910a83e78a98b76ffd4440b7a5ace30
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2024-07-28 21:37:36 -0500

    created dance for couples

---
+++ dance.ts
@@ -0,0 +1,97 @@
+export const dances = [
+	{
+		style: "Jive",
+		link: "https://www.youtube.com/watch?v=nWsGcmgkgr4",
+		description: "Think of it as the Swing but on too much coffee. The Jive consists of two triple steps and a rocking step.Jive is a very happy, boppy, energetic dance, with plenty of knee-lifting, bending, and rocking of the hips. Add in lots of leg pumping action, flicks, and kicks. The man and woman face each other with arms in the closed position and the man leads.",
+		history: "It caught on in the United States in the 1940s and was influenced by the Boogie, Rock & Roll, African/American Swing, and Lindyhop.The name either comes from jive being a form of glib talk or from African dance terms. Jive became a generic term for swing in the United Kingdom."
+	},
+	{
+		style: "Waltz",
+		link: "https://www.youtube.com/watch?v=NBKTN6c_MEQ",
+		description: "Couples travel around the edge of the ballroom in a sweeping swirl of elegant rises and falls.The romantic Waltz is one of the most popular ballroom dances of all time. Considered by some as the mother of present day dances and the backbone dance of the ballroom dancing arena, the Waltz is the basis for many dances.The basic movement of the Waltz is a three-step sequence consisting of a step forward or backward, a step to the side, and a step closing the feet together. The timing of the steps is known as Quick, Quick, Quick or 1,2,3.",
+		history:"The Waltz has continued to develop throughout the 20th century. The Waltz was born as an Austro-German folk dance known as the Landler, which was characterized by the rotating movements of partners dancing together"
+		
+	},
+	{
+		style: "Viennese Waltz",
+		link: "https://www.youtube.com/watch?v=k8gileEp9Yo",
+		description: "The elegance and charm of the Viennese Waltz remind us of glamorous balls in the palaces of Europe. A waltzing couple rotates around the floor, revolving gracefully around each other. The Viennese Waltz is a quick, rotating dance, much faster-paced than the classic, slow Waltz.The following steps are distinctive to the Viennese Waltz: Basic Natural Lilt Step, Basic Reverse Lilt Step, and Stationary Lilt Step",
+		history: "This Waltz developed in Central Europe, originating from the Austrian folk dance known as the Landler The dance arrived in Vienna during the 1800s, then became popular throughout Europe and America."
+	},
+	{
+		style: "Tango",
+		link: "https://www.youtube.com/watch?v=PSTPuzGhhQM",
+		description: "The dance is popular on television and in the films because it beautifully captures a secretly longing or passion can't be openly expressed. The tango a repeated, rigid march around the floor led by an extended arm. A rose between the teeth is optional.Walking to the music while keeping feet grounded and allowing ankles and knees to brush against one another during each step taken. Body weight is kept over the toes and the connection is held between the man and women in the hips.",
+		history:"Spanish settlers were the first to introduce the tango to the New World. Ballroom tango originated in working-class Buenos Aires and the dance spread quickly through Europe during the 1900's, then moved on to the United States. In 1910, tango began gaining popularity in New York."
+	},
+	{
+		style: "Argentine Tango",
+		link: "https://www.youtube.com/watch?v=7qY0_zp58ow",
+		description: "The theatrical, more sensual variant on the Tango Originally from Argentina (go figure), the Argentinian Tango is characterized by a series of kicks and leg hooks that come off as combative, the moves of two people trying to trip each other. Dancers do their best to wrap themselves under each other, holding on for dear life as they move quickly across the dance floor — instantly intimate.",
+		history:"Throughout the years, Argentine dance has become an essential part of the culture of Argentina. The Argentine Tango, enjoyed by tango dancers of all ages, originated in Buenos Aires"
+	},
+	{
+		style: "Paso Doble",
+		link: "https://www.youtube.com/watch?v=nN52QECLr2Q",
+		description: "This is the dance of the bullfighter. The woman usually represents the bullfighter's cape though she can play the role of the bull as well. It's a dramatic and passionate dance performed with impeccable posture and sharp. In Spanish, Paso Doble means two-step and refers to the marching nature of the steps.",
+		history:"The Paso Doble originated in southern France and began gaining popularity in the United States in the 1930s.The steps of the Spanish Paso Doble actually have French names, which is interesting considering its Spanish roots. In France, it was known as the Paso Redoble."
+	},
+
+	{
+		style: "Salsa",
+		link: "https://www.youtube.com/watch?v=aV8dS2m9Adc",
+		description: " The dance is comprised mainly of three \"quick, quick, slow\" steps with a break in between. It's about shifting forward, backwards and to the side, with all the action happening from the hips down. It works well in a contained space like a club, and is relatively easy to learn. Once you've got the basics down, add turns and tricks into the rhythm. The basic Salsa dance rhythm consists of taking three steps for every four beats of music. The odd number of steps creates the syncopation inherent to Salsa dancing and ensures that it takes 8 beats of music to loop back to a new sequence of steps.",
+		history: "The origins of salsa date back to the 1900s in Eastern Cuba, where musical elements and rhythms from various styles were combined. Cuban Son and Afro-Cuban rumba, the two main styles, used diverse musical instruments to create the basis of a rhythm that would later become known as salsa."
+	},
+	{
+		style: "Foxtrot",
+		link: "https://www.youtube.com/watch?v=R90yvxFN5WU",
+		description: "Good foxtrotters look like they're floating on air! The partners are facing one another and frame rotates from one side to another, changing direction after a measure. The dance is flat, with no rise and fall like the waltz. The walking steps are taken as slow for the two beats per steps and quick for one beat per step.",
+		history:"The foxtrot was developed in the United States in the 1920’s and is thought to have been developed in African American nightclubs before being popularized by Vernon and Irene Castle. It is believed to be named after one popularizer, entertainer Harry Fox."
+	},
+	{
+		style: "Charleston",
+		link: "https://www.youtube.com/watch?v=jUqRAUxip4U",
+		description: "This is Gatsby's favorite. It's the dance of the 20s flapper, a rare ballroom phenomenon that can be performed solo and without the traditional hold.The Charleston involves the fast-paced swinging of the legs as well as big arm movements.",
+		history:"Experts in the history of dance believe that some of the Charleston's movements probably came from Trinidad, Nigeria, and Ghana. Its first appearance in the United States was around 1903 in black communities in the South.It did not become internationally popular until the musical Runnin Wild debuted in 1923." 
+	},
+	{
+		style: "Quickstep",
+		link: "https://www.youtube.com/watch?v=eY0VCsmlX60",
+		description: "A fast-paced Foxtrot with some Charleston thrown in, the Quickstep has an old Hollywood glamour. The dancers don't so much glide across the floor as they nimbly prance, as if on clouds that will break at the slightest pressure. It is a fast moving dance, so men are allowed to close their feet and the couples move in short syncopated steps. Quickstep includes the walks, runs, chasses, and turns of the original foxtrot dance, with some other fast figures such as locks, hops, and skips.",
+		history: "The Quickstep was developed in the 1920s in England, though other accounts say it originated in New York. During this time, many bands began playing the Foxtrot at a faster pace, In 1927, however, the Charleston was combined with the Quick Foxtrot resulting in a name that was much too long: the Quick Time Fox Trot and Charleston, so it became known simply as the Quickstep. earning the name Quick Foxtrot."
+	},
+	{
+		style: "Contemporary",
+		link: "https://www.youtube.com/watch?v=nKE11WF34YQ",
+		description: " Contemporary dance tends to combine the strong but controlled legwork of ballet with modern that stresses on torso. Contemporary dance is a style of expressive dance that combines elements of several dance genres including modern, jazz, lyrical and classical ballet. Contemporary dancers strive to connect the mind and the body through fluid dance movements.",
+		history:"During the mid-1940's a student of Martha Graham's, Merce Cunningham, began exploring his own form of dance. Inspired by the radically unique music of John Cage, Cunningham developed an abstract form of dance. Cunningham took dance out of the formal theatrical setting and separated it from the need to express specific stories or ideas. Cunningham introduced the concept that dance movements could be random, and that each performance could be unique. Cunningham, because of his complete break with formal dance techniques, is often referred to as the father of contemporary dance."
+	},
+	
+	{
+		style: "Rumba",
+		link: "https://www.youtube.com/watch?v=rrlq0FnXmhE",
+		description: "This one recounts a steamy romance between a passionate man and a shy, teasing woman.The story of love and emotion! There's plenty of smooth, swaying hip action here as the woman calls the man to her, then rejects him over and over. Like two magnets as one partner's hip moves closer the other partners' hip retreats back.",
+		history:"The Rumba is often referred to as the grandfather of the Latin dances. Originating in Cuba, it first came to the United States in the early 1920s.Before the mambo, salsa and pachanga became popular, Rumba was also known as the style of music commonly heard in Cuba."
+	},
+	{
+		style: "Jazz",
+		link: "https://www.youtube.com/watch?v=5Lc-XZaLZWE",
+		description: "Know it or not, you see jazz dance all over the place, from television shows and movies to music videos and commercials. All jazz dancers interpret and execute moves and steps in their own way. This type of dancing consists of fancy footwork, big leaps, and quick turns. Jazz steps include basic turns, including chaines, piques, pirouettes, jazz turns, and some ballet turns, to name a few.",
+		history:"Jazz dance originated from the African American vernacular dance of the late 1800s to the mid-1900s.Until the middle of 1950s, the term \"jazz dance\" often referred to tap dance, because tap dancing (set to jazz music) was the main performance dance of the era. During the later jazz age, popular forms of jazz dance were the Cakewalk, Black Bottom, Charleston, Jitterbug, Boogie Woogie, Swing dancing and the related Lindy Hop."
+	},
+	
+	
+	{
+		style: "Cha-Cha-Cha",
+		link: "https://www.youtube.com/watch?v=QjcWXpvA5e8",
+		description: "One-two-one-two-three! The quick knee-to-toe action has to be clean for the hips to sway just right. The dance has similar floor patterns to those found in rumba. Think of the Cha-Cha-Cha as the quicker flirty sister to the more serious sensual rumba.many hip rotations and partners synchronising their movements It's a killer dance that works with upbeat songs from every era and place.",
+		history:"Also called the cha-cha, this dance originated in Cuba in the 1940s. Composer and violinist Enrique Jorrín developed the dance as a variant of the mambo and rumba. The name is onomatopoeic, derived from the sound of dancers' shoes as they shuffle around the floor."
+	},
+	{
+		style: "Samba",
+		link: "https://www.youtube.com/watch?v=WiJraXiIihY",
+		description: "The Samba's bounce comes from its \"slow, quick, quick\" step pattern and its shoulder shimmy is a precursor to today's booty shake. Samba is danced with a slight bounce which is created through the bending and straightening the knee.  It's all party - think Carnival in Brazil.",
+		history:"Although there are many types of samba, its defining characteristic is the rhythm.This rhythm was originally derived from the Candomble, or prayer music, in Afro-Brazilian religious practices. In fact, the word “samba” itself means “to pray.\""
+	}
+]
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { dances } from '../../dance'
+import { NgFor } from '@angular/common';
+import { MatCardModule, MatCardHeader } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, MatCardModule, MatCardHeader, MatDividerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'dance';
+  title = 'Dancing For Couples';
+  dances = dances;
+
 }
